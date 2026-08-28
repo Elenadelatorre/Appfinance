3c38188463dcaea6ddff6450522c62e0dbe4e6aa
Elenadelatorre <elenadelatorre1806@gmail.com>
2026-08-28 16:59:50 +0200

    Sanetización de código

--- frontend/src/features/accounts/accounts.js
+++ frontend/src/features/accounts/accounts.js
@@ -1,7 +1,7 @@
 // src/features/accounts/accounts.js
 import { state } from '../../state/state.js';
 import { api } from '../../services/api.js';
-import { $, escapeHtml, clearFileInput } from '../ui/dom.js';
+import { $, escapeHtml, clearFileInput } from '../../ui/dom.js';
 import { showAlert } from '../../utils/toast.js';
 import { openModal, closeModal } from '../../ui/modals.js';
 import {
