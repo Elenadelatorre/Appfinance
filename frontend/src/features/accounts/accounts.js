eb846a52031215640c89c9cd6c9c2a8634e0ec00
Elenadelatorre <elenadelatorre1806@gmail.com>
2026-09-23 22:00:47 +0200

    Load account transactions correctly

--- frontend/src/features/accounts/accounts.js
+++ frontend/src/features/accounts/accounts.js
@@ -13,6 +13,7 @@ import {
 } from '../../utils/visuals.js';
 import {
   annotateTransactionsWithRunningBalances,
+  fetchAllTransactions,
   sortTransactionsByMostRecent,
   renderTxItem,
   openViewTx,
